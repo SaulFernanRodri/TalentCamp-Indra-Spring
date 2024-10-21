cf2ae88e15029e90fd549c3ac69e19f2aa9b06b0
sfernandezrod <sfernandezrod@indra.es>
2024-10-21 12:47:35 +0200

    Initial Commit

--- src/main/java/com/example/talent/repository/ContactRepository.java
+++ src/main/java/com/example/talent/repository/ContactRepository.java
@@ -1,6 +1,6 @@
 package com.example.talent.repository;
 
-import com.example.talent.model.Contact;
+import com.example.talent.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
